dd2438797aef216bd3b9865bb47fa9977a243c07
Argum <Argum@DESKTOP-NEHFTVI>
2022-10-18 11:44:00 -0400

    basic ship format and example (carrier)

---
+++ Battleship/src/Carrier.java
@@ -0,0 +1,10 @@
+
+public class Carrier extends Ship {
+	
+	public Carrier(int l, int x, int y, boolean h)
+	{
+		super(l,x,y,h);
+		name = "Carrier";
+	}
+	
+}
--- Battleship/src/Ship.java
+++ Battleship/src/Ship.java
@@ -8,6 +8,7 @@ public abstract class Ship {
 	private int locX; // tracks X coord of ship's head
 	private int locY; // tracks Y coord of ship's head
 	private boolean isHorizontal;
+	protected String name;
 	
 	public Ship(int l, int x, int y, boolean h)
 	{
@@ -17,6 +18,41 @@ public abstract class Ship {
 		locX = x;
 		locY = y;
 		isHorizontal = h;
+	}
+	
+	public int getLength()
+	{
+		return length;
+	}
+	
+	public int getX()
+	{
+		return locX;
+	}
+	
+	public int getY()
+	{
+		return locY;
+	}
+	
+	public void setX(int newX)
+	{
+		locX = newX;
+	}
+	
+	public void setY(int newY)
+	{
+		locY = newY;
+	}
+	
+	public boolean getIsHorizontal()
+	{
+		return isHorizontal;
+	}
+
+	public void setIsHorizontal(boolean b)
+	{
+		isHorizontal = b;
 	}
 	
 	public boolean isSunk()
