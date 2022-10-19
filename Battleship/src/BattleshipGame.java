a2cef245d6b5ec9f030bb75eafa4aff847044d2e
Sam Blaxberg <rachamimb@gmail.com>
2022-10-19 15:47:44 -0400

    Removed netcode

--- Battleship/src/BattleshipGame.java
+++ Battleship/src/BattleshipGame.java
@@ -1,11 +1,9 @@
-import java.io.IOException;
-
 // (Launcher) Test driver to launch the game.
 
 public class BattleshipGame 
 {
 
-	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException 
+	public static void main(String[] args)
 	{
 		BattleshipModel model = new BattleshipModel();
 		BattleshipView view = new BattleshipView(model);
--- Battleship/src/BattleshipModel.java
+++ Battleship/src/BattleshipModel.java
@@ -10,31 +10,11 @@ public class BattleshipModel
 	private Grid ClientGrid;
 	private GameState state;
 	
-	BattleshipModel() throws ClassNotFoundException, IOException, InterruptedException
+	BattleshipModel()
 	{
 		ServerGrid = new Grid();
 		ClientGrid = new Grid();
 		state = new GameState(2); // starts the game in Setup Mode
-	
-		/*
-		//TODO: Move to separate class
-		Scanner inp = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Enter 0 to be the Server, or 1 to be the client");
-	    int stateInput = inp.nextInt();  // Read user input
-	    Player localPlayer;
-	    if (stateInput == 0)
-	    {
-	    	localPlayer = new PlayerServer();
-	    } 
-	    else if (stateInput == 1)
-	    {
-	   		localPlayer = new PlayerClient();
-	   	}
-	    else
-	    {
-	    	System.out.println("Invalid input. See ya");
-	    }
-	    */
 	}
 	
 }
