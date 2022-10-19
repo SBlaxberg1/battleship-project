65d30c3cbd58fa5de88af6d04ea2d47c004c664c
hyperion <hyperion@localhost>
2022-10-19 14:28:05 -0400

    Added clientPanel

--- Battleship/src/GameWindow.java
+++ Battleship/src/GameWindow.java
@@ -1,52 +1,71 @@
+//Part of BattleshipView.java
+
 import java.awt.*;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
 	
 	JFrame frame;
-	JPanel oceanPanel;
-	JButton[][] ocean = new JButton[10][10];
-	JButton[][] target = new JButton[10][10];
+	JPanel serverPanel;
+	JPanel clientPanel;
+	JButton[][] serverGrid = new JButton[10][10];
+	JButton[][] clientGrid = new JButton[10][10];
 
   public GameWindow() {
       startUp();
-	  //super("Battleship");
-      //initOcean();
   }
   
   public void startUp() {
 	  frame = new JFrame("Battleship");
-	  initOcean();
+	  initServerGrid();
+	  initClientGrid();
 	  frame.setVisible(true);
 	  frame.setSize(1280, 720);
 	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
-  public void initOcean() {
+  public void initServerGrid() {
 	  
-	oceanPanel = new JPanel();
-	oceanPanel.setBounds(0, 0, 500, 500);
-    oceanPanel.setLayout(new GridBagLayout());
+	serverPanel = new JPanel();
+	serverPanel.setBounds(0, 0, 500, 500);
+	serverPanel.setLayout(new GridBagLayout());
     GridBagConstraints c = new GridBagConstraints();
     c.fill = GridBagConstraints.HORIZONTAL;
     
-    //oceanPanel.setBackground(Color.blue);
-    
-    //this.getContentPane().setLayout(new GridLayout(10, 10));
     for (int i=0; i<10; i++) {
     	for (int j=0; j<10; j++) {
-    		ocean[i][j] = new JButton(" ");
-    		ocean[i][j].setText(" ");
-    		ocean[i][j].setBounds(20, 20, 20, 20);
-    		ocean[i][j].setForeground(Color.red);
+    		serverGrid[i][j] = new JButton(" ");
+    		serverGrid[i][j].setText(" ");
+    		serverGrid[i][j].setBounds(0, 0, 20, 20);
     		
     		c.gridx = i;
     		c.gridy = j;
-    		oceanPanel.add(ocean[i][j], c);
+    		serverPanel.add(serverGrid[i][j], c);
     		
     	}
     }
-    frame.add(oceanPanel, BorderLayout.NORTH);
+    frame.add(serverPanel, BorderLayout.NORTH);
+  }
+  
+  public void initClientGrid(){
+	  clientPanel = new JPanel();
+	  clientPanel.setBounds(0,0, 500, 500);
+	  clientPanel.setLayout(new GridBagLayout());
+	  GridBagConstraints c = new GridBagConstraints();
+	  c.fill = GridBagConstraints.HORIZONTAL;
+	  
+	  for (int i=0; i<10; i++) {
+		  for (int j=0; j<10; j++) {
+			  clientGrid[i][j] = new JButton(" ");
+			  clientGrid[i][j].setText(" ");
+			  clientGrid[i][j].setBounds(0, 0, 20, 20);
+			  
+			  c.gridx = i;
+			  c.gridy = j;
+			  clientPanel.add(clientGrid[i][j], c);
+		  }
+	  }
+	  frame.add(clientPanel, BorderLayout.SOUTH);
   }
 
 }
